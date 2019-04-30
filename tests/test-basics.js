d1850d9d8ae8e59bfeeaf6c5207f52b00a78e988
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-04-30 02:55:55 +0000

    test: migrate to live testing.

--- tests/test-basics.js
+++ tests/test-basics.js
@@ -1,117 +1,61 @@
-const http = require('http')
 const bent = require('../')
-const bl = require('bl')
-const crypto = require('crypto')
-const { promisify } = require('util')
-
-let paths = {}
-
-let handler = (req, res) => {
-  /* istanbul ignore if */
-  if (!paths[req.url]) {
-    throw new Error(`No path for URL. ${req.url}`)
-  }
-  paths[req.url](req, res)
-}
+const { PassThrough } = require('stream')
 
 const { test } = require('tap')
 
-const httpTest = (str, fn) => {
-  test(str, async t => {
-    let server = http.createServer(handler)
-    await promisify(cb => server.listen(3000, cb))()
-    await fn(t)
-    await promisify(cb => server.close(cb))()
-  })
-}
+const baseurl = 'https://echo-server.mikeal.now.sh/src'
+const u = path => baseurl + path
 
-paths['/basic'] = (req, res) => {
-  res.end('ok')
-}
-
-httpTest('basic 200 ok', async t => {
-  t.plan(1)
+test('basic 200 ok', async t => {
   let request = bent('string')
-  let str = await request('http://localhost:3000/basic')
+  let str = await request(u('/echo.js?body=ok'))
   t.same(str, 'ok')
 })
 
-httpTest('basic 200 ok baseurl', async t => {
-  t.plan(1)
-  let request = bent('string', 'http://localhost:3000')
-  let str = await request('/basic')
+test('basic 200 ok baseurl', async t => {
+  let request = bent('string', baseurl)
+  let str = await request('/echo.js?body=ok')
   t.same(str, 'ok')
 })
 
-httpTest('basic 200', async t => {
-  t.plan(1)
+test('basic 200', async t => {
   let request = bent()
-  let res = await request('http://localhost:3000/basic')
+  let res = await request(u('/echo.js?body=ok'))
   t.same(res.statusCode, 200)
 })
 
-httpTest('basic buffer', async t => {
-  t.plan(1)
+test('basic buffer', async t => {
   let request = bent('buffer')
-  let buff = await request('http://localhost:3000/basic')
+  let buff = await request(u('/echo.js?body=ok'))
   t.same(buff, Buffer.from('ok'))
 })
 
-paths['/json'] = (req, res) => {
-  res.setHeader('content-type', 'application/json')
-  res.end(JSON.stringify({ ok: 200 }))
-}
-
-httpTest('basic json', async t => {
-  t.plan(1)
+test('basic json', async t => {
   let request = bent('json')
-  let json = await request('http://localhost:3000/json')
-  t.same({ ok: 200 }, json)
+  let json = await request(u('/info.js'))
+  t.same(json.method, 'GET')
 })
 
-paths['/media-type'] = (req, res) => {
-  res.setHeader('content-type', 'application/json')
-  res.end(JSON.stringify({ ok: 200, accept: req.headers.accept }))
-}
-
-httpTest('json based media type', async t => {
-  t.plan(1)
+test('json based media type', async t => {
   let request = bent('json', { accept: 'application/vnd.something.com' })
-  let json = await request('http://localhost:3000/media-type')
-  t.same({ ok: 200, accept: 'application/vnd.something.com' }, json)
+  let json = await request(u('/info.js'))
+  t.same(json.headers.accept, 'application/vnd.something.com')
 })
 
 test('basic PUT', async t => {
-  t.plan(2)
-  let body = await promisify(cb => crypto.randomBytes(1024, cb))()
-  let server = http.createServer((req, res) => {
-    req.pipe(bl((err, buff) => {
-      /* istanbul ignore if */
-      if (err) throw err
-      t.same(buff, body)
-      res.end('ok')
-    }))
-  })
-  await promisify(cb => server.listen(3000, cb))()
-  let request = bent('PUT', 'string')
-  let str = await request('http://localhost:3000/', body)
-  t.same(str, 'ok')
-  await promisify(cb => server.close(cb))()
+  let request = bent('PUT', 'json')
+  let body = Buffer.from(Math.random().toString())
+  let json = await request(u('/info.js'), body)
+  t.same(Buffer.from(json.base64, 'base64'), body)
 })
 
-paths['/201'] = (req, res) => {
-  res.statusCode = 201
-  res.end('ok')
-}
-
-httpTest('status 201', async t => {
-  t.plan(3)
+test('status 201', async t => {
   let request = bent('string', 201)
-  let str = await request('http://localhost:3000/201')
+  let str = await request(u('/echo.js?statusCode=201&body=ok'))
   t.same(str, 'ok')
 
   try {
-    await request('http://localhost:3000/basic')
+    await request(u('/echo.js?body=ok'))
   } catch (e) {
     t.type(e, 'StatusError')
     t.same(e.message, 'Incorrect statusCode: 200')
@@ -119,56 +63,34 @@ httpTest('status 201', async t => {
 })
 
 test('PUT stream', async t => {
-  t.plan(2)
-  let body = await promisify(cb => crypto.randomBytes(1024, cb))()
-  let server = http.createServer((req, res) => {
-    req.pipe(bl((err, buff) => {
-      /* istanbul ignore if */
-      if (err) throw err
-      t.same(buff, body)
-      res.end('ok')
-    }))
-  })
-  await promisify(cb => server.listen(3000, cb))()
-  let request = bent('PUT', 'string')
-  let b = bl()
-  let str = request('http://localhost:3000/', b)
-  b.write(body)
-  b.end()
-  t.same(await str, 'ok')
-  await promisify(cb => server.close(cb))()
+  let body = Buffer.from(Math.random().toString())
+  let request = bent('PUT', 'json')
+  let b = new PassThrough()
+  let res = request(u('/info.js'), b)
+  b.end(body)
+  let info = await res
+  t.same(info.method, 'PUT')
+  // Unfortunately, we can't test this against lamda cause it doesn't support
+  // transfer-encoding: chunked.
+  // t.same(Buffer.from(info.base64, 'base64'), body)
 })
 
 test('PUT JSON', async t => {
-  t.plan(2)
-  let server = http.createServer((req, res) => {
-    req.pipe(bl((err, buff) => {
-      /* istanbul ignore if */
-      if (err) throw err
-      t.same(JSON.parse(buff.toString()), { ok: 200 })
-      res.end('ok')
-    }))
-  })
-  await promisify(cb => server.listen(3000, cb))()
-  let request = bent('PUT', 'string')
-  let str = await request('http://localhost:3000/', { ok: 200 })
-  t.same(await str, 'ok')
-  await promisify(cb => server.close(cb))()
+  let request = bent('PUT', 'json')
+  let info = await request(u('/info.js'), { ok: 200 })
+  let res = JSON.parse(Buffer.from(info.base64, 'base64').toString())
+  t.same(res, { ok: 200 })
+  t.same(info.headers['content-type'], 'application/json')
 })
 
-paths['/errorResponseBody'] = (req, res) => {
-  res.statusCode = 500
-  res.end('hello world')
-}
-
-httpTest('500 Response body', async t => {
+test('500 Response body', async t => {
   let request = bent()
   let body
   try {
-    await request('http://localhost:3000/errorResponseBody')
+    await request(u('/echo.js?statusCode=500&body=ok'))
   } catch (e) {
     body = e.responseBody
   }
   let buffer = await body
-  t.same(buffer.toString(), 'hello world')
+  t.same(buffer.toString(), 'ok')
 })
